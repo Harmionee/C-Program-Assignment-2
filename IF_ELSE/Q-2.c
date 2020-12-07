ebd0baa35b3f67d6c57c8e3ee7d52fcc5a92c369
Harmionee Kaur <harmioneekaur@gmail.com>
2020-12-07 22:45:55 +0530

    second commit

---
+++ IF_ELSE/Q-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char a;
+    printf("Enter a character \n");
+    scanf("%c",&a);
+    if(isupper(a)!=0)
+    {
+        printf("The charecter is UPPERCASE");
+    }
+    else {  printf("The charecter is LOWERCASE"); }
+    return 0;
+}
